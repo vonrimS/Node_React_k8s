cd6c510f9155a1c5bf928c08a7efb5ec37c2d612
vonrimS <mr.mytab@gmail.com>
2023-02-07 21:28:42 +0100

    Fetching data for client service added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
     return <div className='container'>
         <h1>Create Post</h1>
-        <hr />
         <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
+
     </div>;
 };
