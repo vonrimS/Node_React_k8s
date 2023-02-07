c76a63dc48a771bb076c48f4d8223847d8ae4bea
vonrimS <mr.mytab@gmail.com>
2023-02-07 22:34:59 +0100

    Comment list fetch for client service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 
 export default () => {
@@ -23,6 +24,7 @@ export default () => {
             key={post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
+                <CommentList postId={post.id} />
                 <CommentCreate postId={post.id} />
             </div>
         </div>;
