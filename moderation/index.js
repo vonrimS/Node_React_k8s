c596e40d9869a8df46334754c1ef287acf3e33eb
vonrimS <mr.mytab@gmail.com>
2023-02-08 11:41:15 +0100

    updating comment content

--- moderation/index.js
+++ moderation/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = new express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/events', async (req, res) => {
     const { type, data } = req.body;
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
-        await axios.posts('http://localhost:4005/events', {
+        await axios.post('http://localhost:4005/events', {
             type: 'CommentModerated',
             data: {
                 id: data.id,
@@ -20,7 +22,7 @@ app.post('/events', async (req, res) => {
         });
     }
 
-    res.send({})
+    res.send({});
 });
 
 app.listen(4003, () => {
