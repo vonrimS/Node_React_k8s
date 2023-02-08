403afee5705a10a4b5926682dd43b3986be59fea
vonrimS <mr.mytab@gmail.com>
2023-02-08 02:30:03 +0100

    moderation service logic improved

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
     commentsByPostId[req.params.id] = comments;
 
     axios.post('http://localhost:4005/events', {
@@ -27,7 +27,8 @@ app.post('/posts/:id/comments', (req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -31,6 +31,13 @@ app.post('/events', (req, res) => {
             console.log(err.message);
         });
 
+    // moderation
+    axios
+        .post('http://localhost:4003/events', event)
+        .catch((err) => {
+            console.log(err.message);
+        });
+
     res.send({ status: 'OK' });
 });
 
